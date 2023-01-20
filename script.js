c788b792fd21e386823db733e8281a236e7d804d
Solomon Lambert <sol.lambert@gmail.com>
2023-01-19 18:01:52 -0600

    add proper functionality for player death when out of AP

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ $(document).ready(onReady);
 // let fungusHP = 100;
 let fungusHP = 100;
 let playerAP = 100;
+let playerAlive = true;
 let fungusAlive = true;
-let arcanceSceptre = {name: 'arcane-sceptre', damage: 14, cost: 12};
-let entangle = {name: 'entangle', damage: 9, cost: 23};
-let dragonBlade = {name: 'dragon-blade', damage: 47, cost: 38};
-let starFire = {name: 'star-fire', damage: 25, cost: 33};
+const arcaneSceptre = {name: 'arcane-sceptre', damage: 14, cost: 12};
+const entangle = {name: 'entangle', damage: 9, cost: 23};
+const dragonBlade = {name: 'dragon-blade', damage: 47, cost: 38};
+const starFire = {name: 'star-fire', damage: 25, cost: 33};
 
 function onReady() {
     
@@ -34,13 +35,39 @@ function render() {
         fungus.removeClass('walk');
         fungus.addClass('dead');
     }
+    if (!playerAlive) {
+        let fungus = $('.freaky-fungus');
+        fungus.removeClass('walk');
+        fungus.addClass('jump');
+    }
+
+    if (arcaneSceptre.cost > playerAP) {
+        $('.arcane-sceptre').prop('disabled', true);
+    } else {
+        $('.arcane-sceptre').prop('disabled', false);
+    }
+    if (entangle.cost > playerAP) {
+        $('.entangle').prop('disabled', true);
+    } else {
+        $('.entangle').prop('disabled', false);
+    }
+    if (dragonBlade.cost > playerAP) {
+        $('.dragon-blade').prop('disabled', true);
+    } else {
+        $('.dragon-blade').prop('disabled', false);
+    }
+    if (starFire.cost > playerAP) {
+        $('.star-fire').prop('disabled', true);
+    } else {
+        $('.star-fire').prop('disabled', false);
+    }
 }
 
 function onAttack() {
     let attack = $(this);
 
     if (attack.hasClass('arcane-sceptre')) {
-        attack = arcanceSceptre;
+        attack = arcaneSceptre;
     } else if (attack.hasClass('entangle')) {
         attack = entangle;
     } else if (attack.hasClass('dragon-blade')) {
@@ -54,9 +81,17 @@ function onAttack() {
     } else {
         console.log('not enough ap')
     }
+    if (playerAP < arcaneSceptre.cost
+        && playerAP < entangle.cost
+        && playerAP < dragonBlade.cost
+        && playerAP < starFire.cost) {
+            playerAlive = false;
+        }
 
     if (fungusHP <= 0){
         fungusAlive = false;
+    } else if (playerAP <= 0) {
+        playerAlive = false;
     }
     render();
 }
